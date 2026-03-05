d22fcc232929ad787936c6615b39bf8fd8e32922
erasels <emanuelbedburdick@yahoo.de>
2026-03-05 20:50:38 +0100

    feat: Add hover tooltips for ascenscion modifiers (kiooeht)

---
+++ MintySpire2/src/AscHoverTooltips.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+
+namespace MintySpire2.MintySpire2.src;
+
+using Godot;
+using HarmonyLib;
+using MegaCrit.Sts2.Core.Helpers;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Nodes.HoverTips;
+using MegaCrit.sts2.Core.Nodes.TopBar;
+using MegaCrit.Sts2.Core.Runs;
+
+/**
+ * Credits to kiooeht, this displays tooltips for each active ascension when hovering the player icon in the topbar.
+ */
+
+[HarmonyPatch]
+public static class AscHoverTooltips
+{
+    private static List<IHoverTip> _myTips = new List<IHoverTip>();
+
+    [HarmonyPatch(typeof(NTopBarPortraitTip), nameof(NTopBarPortraitTip.Initialize))]
+    public class InitializeTip
+    {
+        [HarmonyPostfix]
+        public static void Init(IRunState runState, IHoverTip ____hoverTip)
+        {
+            _myTips.Add(____hoverTip);
+            for (int i = 1; i <= runState.AscensionLevel; ++i)
+            {
+                _myTips.Add(new HoverTip(
+                    AscensionHelper.GetTitle(i),
+                    AscensionHelper.GetDescription(i)
+                ));
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(NTopBarPortraitTip), "OnHovered")]
+    public class ShowTip
+    {
+        [HarmonyPrefix]
+        public static bool Prefix(NTopBarPortraitTip __instance, bool ____showTip, IHoverTip ____hoverTip)
+        {
+            if (!____showTip)
+                return false;
+            NHoverTipSet.CreateAndShow(__instance, _myTips).GlobalPosition = __instance.GlobalPosition + new Vector2(0, __instance.Size.Y + 20);
+
+            return false;
+        }
+    }
+}
