d3271fe17a20df99217b71bf9d5846ae6d57fb86
Mangochicken <mangochkn@gmail.com>
2026-03-15 15:27:51 +0800

    Adding in my Pael's Looking Eye mod
    Changes from the original are:
    - Renaming the follow mouse gdscript file to match the scene
    - Update the directories, classes, and paths to the new mod name
    - slight cleanup in both code files
    - rename the replacement eye sprite from `paels_eye` to `paels_eye_base`
    
    and also the .import and .uid files aren't included (may break the
    .tscn? it should fallback to the given paths ideally)
    - Alternative to this is to include these files in the repo, though this
    may be undisireable depending on how the pack looks

---
+++ MintySpire2/scenes/looking_eye/looking_eye.gd
@@ -0,0 +1,24 @@
+extends TextureRect
+
+var parent: Control
+var parent_rect: Rect2
+var screen_rect: Rect2
+var rect_ratio: Vector2
+
+func _ready() -> void:
+	parent = get_parent()
+	if parent:
+		parent_rect = parent.get_rect()
+		screen_rect = get_viewport_rect()
+		parent.item_rect_changed.connect(_on_item_rect_changed)
+		rect_ratio = Vector2(parent_rect.size.x / screen_rect.size.x, parent_rect.size.y / screen_rect.size.y)
+
+func _on_item_rect_changed() -> void:
+	screen_rect = get_viewport_rect()
+	rect_ratio = Vector2(parent_rect.size.x / screen_rect.size.x, parent_rect.size.y / screen_rect.size.y)
+
+func _process(_delta: float) -> void:
+	var mouse_pos := parent.get_global_mouse_position()
+	var relative_mouse_pos := (parent.global_position + parent.size / 2) - Vector2(mouse_pos)
+	var transformed_relative_offset := Vector2(relative_mouse_pos.x * rect_ratio.x, relative_mouse_pos.y * rect_ratio.y)
+	position = -transformed_relative_offset * (size.x / 300.0) # This is 0.2 at the inventory size
---
+++ src/PaelsEyePatch.cs
@@ -0,0 +1,130 @@
+using Godot;
+using HarmonyLib;
+using MegaCrit.Sts2.Core.Entities.UI;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Relics;
+using MegaCrit.Sts2.Core.Nodes.Events;
+using MegaCrit.Sts2.Core.Nodes.Relics;
+using MegaCrit.Sts2.Core.Nodes.Screens.InspectScreens;
+using MegaCrit.Sts2.Core.Nodes.Screens.RelicCollection;
+
+namespace MintySpire2;
+
+[HarmonyPatch]
+public class PaelsLookingEyePatch()
+{
+    private static Texture2D? PaelsEyeBase {
+        get {
+            var loaded = ProjectSettings.LoadResourcePack(OS.GetExecutablePath().GetBaseDir().PathJoin("mods/MintySpire2/MintySpire2.pck"));
+            if (loaded) {
+                var eye = ResourceLoader.Load<Texture2D>("res://MintySpire2/images/relics/paels_eye_base.png");
+                return eye;
+            }
+            else {
+                MainFile.Logger.Info("Oh my god its broken again");
+            }
+
+            return null;
+        }
+    }
+
+    private static PackedScene LookingEyeScene => ResourceLoader.Load<PackedScene>("res://MintySpire2/scenes/looking_eye/looking_eye.tscn");
+
+    private const string _LookingEyeNode = "LookingEye";
+
+    // Inventory visuals
+    [HarmonyPatch(typeof(NRelicInventoryHolder), nameof(NRelicInventoryHolder._Ready))]
+    [HarmonyPostfix]
+    static void OnInventoryRelicReady(NRelicInventoryHolder __instance)
+    {
+        var relic = __instance.Relic;
+        var model = relic.Model;
+
+        if (model is PaelsEye)
+        {
+            var icon_node = relic.Icon;
+
+            icon_node.Texture = PaelsEyeBase;
+
+            var scene_instance = LookingEyeScene.Instantiate();
+            icon_node.AddChild(scene_instance);
+            scene_instance.Owner = icon_node;
+        }
+    }
+
+    // Holder for shops i think??
+    [HarmonyPatch(typeof(NRelicBasicHolder), nameof(NRelicBasicHolder._Ready))]
+    [HarmonyPostfix]
+    static void OnBaseRelicReady(NRelicBasicHolder __instance)
+    {
+        var relic = __instance.Relic;
+        var model = relic.Model;
+
+        if (model is PaelsEye)
+        {
+            var icon_node = relic.Icon;
+            icon_node.AddChild(LookingEyeScene.Instantiate());
+        }
+    }
+
+    // Ancient Event visual
+    [HarmonyPatch(typeof(NEventOptionButton), nameof(NEventOptionButton._Ready))]
+    [HarmonyPostfix]
+    static void OnEventOptionButtonReady(NEventOptionButton __instance)
+    {
+        if (__instance.Event is AncientEventModel && __instance.Option.Relic is PaelsEye)
+        {
+            var icon_node = __instance.GetNode<TextureRect>("%RelicIcon");
+            icon_node.SizeFlagsVertical = Control.SizeFlags.ShrinkCenter;
+            icon_node.Texture = PaelsEyeBase;
+            icon_node.AddChild(LookingEyeScene.Instantiate());
+        }
+    }
+
+    // Relic Collection
+    [HarmonyPatch(typeof(NRelicCollectionEntry), nameof(NRelicCollectionEntry._Ready))]
+    [HarmonyPostfix]
+    static void OnRelicCollectionEntryReady(NRelicCollectionEntry __instance)
+    {
+        if (__instance.relic is PaelsEye && __instance.ModelVisibility == ModelVisibility.Visible)
+        {
+            var icon_node = __instance._relicNode.GetChild(0) as TextureRect;
+            icon_node.Texture = PaelsEyeBase;
+            icon_node.AddChild(LookingEyeScene.Instantiate());
+        }
+    }
+
+    // Large inspect relic view
+    [HarmonyPatch(typeof(NInspectRelicScreen), nameof(NInspectRelicScreen._Ready))]
+    [HarmonyPostfix]
+    static void OnInspectRelicReady(NInspectRelicScreen __instance)
+    {
+        var icon_node = __instance._relicImage;
+        icon_node.AddChild(LookingEyeScene.Instantiate());
+    }
+
+    [HarmonyPatch(typeof(NInspectRelicScreen), nameof(NInspectRelicScreen.UpdateRelicDisplay))]
+    [HarmonyPostfix]
+    static void OnInspectRelicRedraw(NInspectRelicScreen __instance)
+    {
+        var relicModel = __instance._relics[__instance._index];
+        var icon_node = __instance._relicImage;
+        var is_paels_eye = false;
+        if (relicModel is PaelsEye)
+        {
+            // Not overwriting the base file, ideally allows toggling individual sections.
+            icon_node.Texture = PaelsEyeBase;
+            is_paels_eye = true;
+        }
+        else
+        {
+            is_paels_eye = false;
+        }
+
+        var child = icon_node.GetNode<TextureRect>(_LookingEyeNode);
+        if (child != null)
+        {
+            child.Set("visible", is_paels_eye);
+        }
+    }
+}
